23e3ff5850a0b9bb6fc7e55c9e303692c69bc187
Mila Desmet <desmetmila@gmail.com>
2023-08-08 15:26:13 +0200

    Mise en place des fichiers Accueil.tsx, ContactForm.tsx et Footer.tsx

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const ContactForm = () => {
+
+   
+    return(
+    <div>
+       
+    </div>
+    )
+}
---
+++ src/screens/Accueil.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Accueil = () => {
+  return (
+    <main>
+     
+    </main>
+  );
+};
