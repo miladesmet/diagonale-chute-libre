45089937d79c97afdcf8a077f38742390bb2eb39
Mila Desmet <desmetmila@gmail.com>
2023-08-18 00:18:35 +0200

    Bug chemin

--- src/components/ContactMentionsFooter.tsx
+++ src/components/ContactMentionsFooter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import logo from './assets/logo.png'
-import facebook from '../assets/facebook.png'
-import insta from '../assets/instagram.png'
+import logo from '../assets/logo.png';
+import facebook from '../assets/facebook.png';
+import insta from '../assets/instagram.png';
 
 const ContactMentionsFooter = () => {
     return (
