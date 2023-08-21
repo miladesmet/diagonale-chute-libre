ff9478c8140e443cf2406e7ac00dc6dcfd3b079c
Mila Desmet <desmetmila@gmail.com>
2023-08-21 15:18:36 +0200

    Modif rewiev

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Accueil from '../screens/Accueil'
 import { Shop } from '../screens/Shop'
 import {FAQ} from '../screens/FAQ'
+import {ArticleDetail} from '../components/ArticleDetail'
+
 const routes = [
   { path: '/', title: 'Accueil', element: <Accueil /> },
   { path: '/boutique', title: 'Boutique', element: <Shop /> },
   { path: '/galerie', title: 'Galerie photo', element: <>ToDo</> },
   { path: '/panier', title: 'Panier d’achat', element: <>ToDo</> },
   { path: '/FAQ', title: 'FAQ', element: <FAQ/> },
+  { path: '/boutique/:id', element: <ArticleDetail/> },
 ]
 
 export { routes }
